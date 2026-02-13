46b25620db862108b876461d76aac4a5eb1fa5e6
thefreemavens <thefreemavens@gmail.com>
2026-02-13 11:34:47 +0200

    Add GH Issue Templates and config ++

---
+++ app/components/AppLogo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="
+        flex
+        justify-center
+        items-center
+        aspect-square
+        size-48 md:size-64
+        rounded-4xl md:rounded-[4rem]
+        bg-linear-to-b from-neutral-600 to-transparent
+        p-px
+      "
+  >
+    <span
+      class="
+          flex
+          justify-center
+          items-center
+          mx-auto
+          size-full
+          drop-shadow-xl
+          drop-shadow-black/40
+          rounded-[calc(2rem-1px)] md:rounded-[calc(4rem-1px)]
+          bg-linear-to-b from-neutral-800 to-neutral-950
+        "
+    >
+      <UIcon
+        name="i-local-tfm"
+        class="size-4/7"
+      />
+    </span>
+  </div>
+</template>
